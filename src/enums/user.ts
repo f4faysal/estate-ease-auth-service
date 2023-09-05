d1e5a687638736b039aaa284d6e01b5424e98d1b
f4faysal <f4faysals@gmail.com>
2023-09-05 21:42:15 +0600

    auth created

--- src/enums/user.ts
+++ src/enums/user.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export enum ENUM_USER_ROLE {
-  SUPER_ADMIN = 'super_admin',
-  ADMIN = 'admin',
-  SELLER = 'seller',
-  RENTUSER = 'rentuser',
+  SUPER_ADMIN = 'Super_Admin',
+  ADMIN = 'Admin',
+  HOMEOWNER = 'HomeOwner',
+  RENTUSER = 'RentUser',
 }
