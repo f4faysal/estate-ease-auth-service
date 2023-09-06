5b65600da58f5a738d072c186443141318632051
f4faysal <f4faysals@gmail.com>
2023-09-06 16:41:33 +0600

    Login Done

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -13,10 +13,6 @@ import {
 
 const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
   const { id, password } = payload;
-  // creating instance of User
-  // const user = new User();
-  //  // access to our instance methods
-  //   const isUserExist = await user.isUserExist(id);
 
   const isUserExist = await User.isUserExist(id);
 
@@ -31,7 +27,7 @@ const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
     throw new ApiError(httpStatus.UNAUTHORIZED, 'Password is incorrect');
   }
 
-  //create access token & refresh token
+  //!create access token & refresh token
 
   const { id: userId, role, needsPasswordChange } = isUserExist;
   const accessToken = jwtHelpers.createToken(
@@ -54,7 +50,7 @@ const loginUser = async (payload: ILoginUser): Promise<ILoginUserResponse> => {
 };
 
 const refreshToken = async (token: string): Promise<IRefreshTokenResponse> => {
-  //verify token
+  //!verify token
   // invalid token - synchronous
   let verifiedToken = null;
   try {
@@ -67,7 +63,7 @@ const refreshToken = async (token: string): Promise<IRefreshTokenResponse> => {
   }
 
   const { userId } = verifiedToken;
-
+  console.log(verifiedToken);
   // tumi delete hye gso  kintu tumar refresh token ase
   // checking deleted user's refresh token
 
