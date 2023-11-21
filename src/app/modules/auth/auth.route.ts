468eb926c8bbc60747865685457152f0ab8d2df4
f4faysal <f4faysals@gmail.com>
2023-11-21 20:10:12 +0600

    Fix import statement in auth.route.ts

--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthController } from './auth.controller';
 import { AuthValidation } from './auth.validation';
+
 const router = express.Router();
 
 router.post(
