ed1cc0fb1f55b9a7295e8d778aeae8c56c8781b3
f4faysal <f4faysals@gmail.com>
2023-11-21 20:05:16 +0600

    Fix import statement in homeInfo.Controller.ts

--- src/app/modules/homeInfo/homeInfo.model.ts
+++ src/app/modules/homeInfo/homeInfo.model.ts
@@ -1,65 +1,7 @@
 import { Schema, Types, model } from 'mongoose';
+
 import { locationsEnaum, residential, statusEnaum } from './homeInfo.constant';
 import { HomeInfoModel, IHomeInfo } from './homeInfo.interface';
-
-// Define the schema for IHomeFeatures
-// const HomeFeaturesSchema = new Schema<IHomeFeatures>({
-//   hasGarden: { type: Boolean },
-//   hasGarage: { type: Boolean },
-//   hasInternet: { type: Boolean },
-//   hasSecurity: { type: Boolean },
-//   hasCleaning: { type: Boolean },
-//   hasAirConditioning: { type: Boolean },
-// });
-
-// // Define the schema for IHomeSige
-// const HomeSizeSchema = new Schema<IHomeSige>({
-//   StringOfRooms: { type: String },
-//   StringOfBathrooms: { type: String },
-//   StringOfBalconies: { type: String },
-//   StringOfWindos: { type: String },
-//   StringOfFloors: { type: String },
-//   sizePerUnit: { type: String },
-//   totalSQFT: { type: String },
-// });
-
-// // Define the schema for IReview
-// const ReviewSchema = new Schema<IReview>({
-//   rating: { type: String, required: true },
-//   review: [{ type: String }],
-// });
-
-// // Define the schema for IQuestion
-// const QuestionSchema = new Schema<IQuestion>({
-//   question: { type: String, required: true },
-//   answers: [{ type: String }],
-// });
-
-// // Define the schema for IHomeDetails
-// const HomeDetailsSchema = new Schema<IHomeDetails>({
-//   title: { type: String, required: true },
-//   tageLine: { type: String, required: true },
-//   tages: [{ type: String }],
-//   price: { type: String, required: true },
-//   offerPrice: { type: String },
-//   address: { type: String, required: true },
-//   description: { type: String, required: true },
-//   images: [{ type: String }],
-//   homeType: { type: String, required: true },
-//   homeSize: { type: String, required: true },
-//   Location: {
-//     type: String,
-//     enum: [
-//       'Demra', 'Dhaka Cantt.', 'Dhamrai', 'Dhanmondi', 'Gulshan', 'Jatrabari',
-//       'Joypara', 'Keraniganj', 'Khilgaon', 'Khilkhet', 'Lalbag', 'Mirpur',
-//       'Mohammadpur', 'Motijheel', 'Nawabganj', 'New Market', 'Palton', 'Ramna',
-//       'Sabujbag', 'Savar', 'Sutrapur', 'Tejgaon', 'Tejgaon Industrial Area', 'Uttara'
-//     ],
-//     required: true,
-//   },
-//   homeSizeDetails: { type: HomeSizeSchema },
-//   features: { type: HomeFeaturesSchema },
-// });
 
 const HomeSchema = new Schema<IHomeInfo, HomeInfoModel>(
   {
--- src/app/modules/homeInfo/homeInfo.route.ts
+++ src/app/modules/homeInfo/homeInfo.route.ts
@@ -1,27 +1,10 @@
 import express from 'express';
+
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
 import { HomeInfoController } from './homeInfo.Controller';
 
 const router = express.Router();
-
-// router.get(
-//   '/:id',
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   AdminController.getSingleAdmin
-// );
-// router.get('/', AdminController.getAllAdmins);
-// router.patch(
-//   '/:id',
-//   validateRequest(AdminValidation.updateAdmin),
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-//   AdminController.updateAdmin
-// );
-// router.delete(
-//   '/:id',
-//   auth(ENUM_USER_ROLE.SUPER_ADMIN),
-//   AdminController.deleteAdmin
-// );
 
 router.post(
   '/',
